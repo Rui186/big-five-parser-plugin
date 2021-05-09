01a5eddfe4ccfc18385e27b11322779143135b2f
Rui <rui1765428404@gmail.com>
2021-05-09 10:27:25 +0930

    tweak submitter method name

--- lib/big_five_parser/submitter.rb
+++ lib/big_five_parser/submitter.rb
@@ -2,7 +2,7 @@ module BigFiveParser
   class Submitter
     require 'faraday'
 
-    def self.submit(params)
+    def self.submit_to_recruitbot(params)
       url = 'https://recruitbot.trikeapps.com/api/v1/roles/bellroy-tech-team-recruit/big_five_profile_submissions'
       response = Faraday.post(url, params, "Content-Type" => "application/json")
       { status: response.status, body: response.body }
